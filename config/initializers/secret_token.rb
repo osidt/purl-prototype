6f061ddbd94e8775a1168658e91453998ff2e9c4
James Ottaway <james@ottaway.mp>
2011-03-09 16:09:49 +1000

    Ahh, that new Rails app smell...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purl::Application.config.secret_token = '957cf0497bfb407fb541c95372302e554a17ecc7412a98156d84dee25d58969e3837c86c683761784f528f457a5ee1be7346a911a6c0bd17f12ce8d422763177'
